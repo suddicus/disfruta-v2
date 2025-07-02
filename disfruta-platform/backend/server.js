959575378a49679cc757713cefb5bb818acf1681
suddicus <suhdenzo04@gmail.com>
2025-07-02 21:05:52 +0100

    fixes: workaroud for cors

--- disfruta-platform/backend/server.js
+++ disfruta-platform/backend/server.js
@@ -29,7 +29,7 @@ connectDB();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,// || 'http://localhost:5173',
   credentials: true
 }));
 
